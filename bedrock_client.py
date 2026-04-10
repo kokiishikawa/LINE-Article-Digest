6e1c20d46903078ff565848cbdf52be0b33e77f5
koki <137967060+kokiishikawa@users.noreply.github.com>
2026-04-10 17:52:06 +0900

    fix: bedrock設定変更

--- bedrock_client.py
+++ bedrock_client.py
@@ -1,7 +1,7 @@
 import boto3
 
 def summarize_article(html: str) -> str:
-    bedrock = boto3.client("bedrock-runtime", region_name="us-east-1")
+    bedrock = boto3.client("bedrock-runtime", region_name="ap-northeast-1")
 
     prompt = f"""
         以下の内容を要約して
@@ -9,13 +9,17 @@ def summarize_article(html: str) -> str:
     """
 
     resp = bedrock.converse(
-        modelId="anthropic.claude-3-5-haiku-20241022-v1:0",
+        modelId="ap.anthropic.claude-haiku-4-5-20251001-v1:0",
         messages=[
             {
                 "role": "user",
                 "content": [{"text": prompt}]
             }
-        ]
+        ],
+        inferenceConfig={
+            "maxTokens": 1024,
+            "temperature": 0.5,
+        }
     )
 
     answer = resp["output"]["message"]["content"][0]["text"]
